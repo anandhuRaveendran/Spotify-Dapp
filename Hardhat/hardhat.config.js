51d6d5834923556c9332f48ab9b91ab9d613db0b
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-09-13 16:34:46 +0530

    private key updated

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
       url:"http://127.0.0.1:8545/"
     },
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/eloqM4tZVjsuFEenH-bfqhnMqINwW8m0",
-      accounts:["c28bfcfe9e5ac6dfaf8b7ab5886abdacdb9ab8773ab024a775677ea101a88bb9"]
+      url: "infura rpc",
+      accounts:["private key"]
     }
   },
   solidity: "0.8.24",
