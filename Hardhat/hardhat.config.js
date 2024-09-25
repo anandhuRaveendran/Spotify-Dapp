fcb3a5651c9594498db20d4a0001b418d6bd0286
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-09-25 12:15:00 +0530

    address removed

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomicfoundation/hardhat-ignition")
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,8 +8,8 @@ module.exports = {
       url:"http://127.0.0.1:8545/"
     },
     sepolia: {
-      url: "https://sepolia.infura.io/v3/15afd28d8e0e450b800bd4e53b6e982f",
-      accounts:["c28bfcfe9e5ac6dfaf8b7ab5886abdacdb9ab8773ab024a775677ea101a88bb9"]
+      url: "infura rpc",
+      accounts:["private key"]
     }
   },
   solidity: "0.8.24",
